1db95041ce34612db95c09bc9b548ec90b47495f
abdullahshaker10 <abdullah.shaker2017@gmail.com>
2021-01-21 02:23:51 +0200

    Signup and login

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-
+from django.urls import reverse, resolve
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
@@ -10,8 +10,6 @@ class CustomUserTests(TestCase):
         )
         self.assertEqual(user.username, "abdullah")
         self.assertEqual(user.email, "abdullah@email.com")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
     def test_create_superuser(self):
@@ -21,6 +19,21 @@ class CustomUserTests(TestCase):
         )
         self.assertEqual(admin_user.username, "superadmin")
         self.assertEqual(admin_user.email, "superadmin@email.com")
-        self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignupTests(TestCase):
+    username = "newuser"
+    email = "newuser@email.com"
+    
+
+    def setUp(self):
+        url = reverse("account_signup")
+        self.response = self.client.get(url)
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.last().username, self.username)
+        self.assertEqual(get_user_model().objects.last().email, self.email)
